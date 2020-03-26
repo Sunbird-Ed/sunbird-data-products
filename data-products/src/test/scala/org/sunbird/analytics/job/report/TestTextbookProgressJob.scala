d475f5559267efcfc2849e743f515f948d1eb409
Utkarsha Kapoor <utkit14@gmail.com>
2020-03-26 18:25:55 +0530

    Issue #SB-17658- textbook progress job testcases resolved

--- data-products/src/test/scala/org/sunbird/analytics/job/report/TestTextbookProgressJob.scala
+++ data-products/src/test/scala/org/sunbird/analytics/job/report/TestTextbookProgressJob.scala
@@ -8,27 +8,15 @@ import org.sunbird.analytics.util.SparkSpec
 class TestTextbookProgressJob extends SparkSpec(null) {
 
   "TextbookProgressJob" should "execute and won't throw any errors/exception" in {
-<<<<<<< HEAD
     val reportConf = ReportConfig("content_progress", "", QueryDateRange(None, None, None),List(),
       Map("review" -> "Review", "subject" -> "Subject", "application_html" -> "Uploaded Interactive Content", "resourceType" -> "Content Type",
         "gradeLevel" -> "Grade", "vedio_youtube" -> "YouTube Content", "draft" -> "Draft", "video_mp4" -> "Uploaded Videos", "medium" -> "Medium",
         "creator" -> "Created By", "board" -> "Board", "live" -> "Live", "status" -> "Status", "application_ecml" -> "Created on Diksha", "application_pdf" -> "Text Content"),
       List(OutputConfig("csv",None,List(),List(),List())))
-=======
-      val reportConf = ReportConfig("content_progress", "", QueryDateRange(None, None, None),List(),
-        Map("review" -> "Review", "subject" -> "Subject", "application_html" -> "Uploaded Interactive Content", "resourceType" -> "Content Type",
-          "gradeLevel" -> "Grade", "vedio_youtube" -> "YouTube Content", "draft" -> "Draft", "video_mp4" -> "Uploaded Videos", "medium" -> "Medium",
-          "creator" -> "Created By", "board" -> "Board", "live" -> "Live", "status" -> "Status", "application_ecml" -> "Created on Diksha", "application_pdf" -> "Text Content"),
-        List(OutputConfig("csv",None,List(),List(),List())))
->>>>>>> c1886a1435a58a3c491fb983b19fc2c225c79513
 
     val modelParams = Map("reportConfig" -> reportConf, "bucket" -> "test-container", "key" -> "druid-reports/", "filePath" -> "src/test/resources/")
     val con = JobConfig(Fetcher("none",None,None,None),None,None,"org.ekstep.analytics.model.TextbookProgressModel",Option(modelParams),Option(Array(Dispatcher("console", Map("printEvent" -> false.asInstanceOf[AnyRef])))),Option(8),Option("Content Progress Metrics Model"),Option(false),None,None)
 
     TextbookProgressJob.main(JSONUtils.serialize(con))(Option(sc))
   }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> c1886a1435a58a3c491fb983b19fc2c225c79513
