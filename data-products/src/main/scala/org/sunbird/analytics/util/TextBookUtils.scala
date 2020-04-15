dfb44b64164744eee7ef3ffaf55d10e1f78c739c
Isha Wakankar <iwakankar@hotmail.com>
2020-04-15 12:24:53 +0530

    .

--- data-products/src/main/scala/org/sunbird/analytics/util/TextBookUtils.scala
+++ data-products/src/main/scala/org/sunbird/analytics/util/TextBookUtils.scala
@@ -229,7 +229,7 @@ object TextBookUtils {
           if(null != parsedData.dialcodes) { dialcodes = parsedData.dialcodes(0) :: dialcodes }
           levelNames = parsedData.name :: levelNames
         }
-        if(parsedData.children.nonEmpty || parsedData.children.get.isEmpty) {
+        if(parsedData.children.getOrElse(List()).nonEmpty) {
           println(parsedData.children)
           println(parsedData.children.size)
           parsedData = parsedData.children.get(parsedData.children.size-1)
