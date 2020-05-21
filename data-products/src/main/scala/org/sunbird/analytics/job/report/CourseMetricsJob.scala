4aad6406b256a26a34307d9f3f9b224ee3113a31
Manjunath Davanam <manjunathd@ilimi.in>
2020-05-21 14:18:40 +0530

    Issue #000 fix:  filtered the columns while loading the user data

--- data-products/src/main/scala/org/sunbird/analytics/job/report/CourseMetricsJob.scala
+++ data-products/src/main/scala/org/sunbird/analytics/job/report/CourseMetricsJob.scala
@@ -237,7 +237,8 @@ object CourseMetricsJob extends optional.Application with IJob with ReportGenera
       .join(resolvedOrgNameDF, Seq("userid", "rootorgid"), "left_outer")
       .dropDuplicates("userid")
       .cache();
-    resolvedData.count()
+//    resolvedData.count()
+//    resolvedData
     resolvedData
   }
 
