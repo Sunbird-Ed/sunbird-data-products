7a677a99cfd7122ab9b21b930d42406f88cc1e17
Manjunath Davanam <manjunathd@ilimi.in>
2020-05-22 15:43:13 +0530

    Issue #000 fix: content ids

--- data-products/src/main/scala/org/sunbird/analytics/job/report/AssessmentMetricsJob.scala
+++ data-products/src/main/scala/org/sunbird/analytics/job/report/AssessmentMetricsJob.scala
@@ -236,6 +236,7 @@ object AssessmentMetricsJob extends optional.Application with IJob with BaseRepo
    */
   def denormAssessment(report: DataFrame)(implicit spark: SparkSession): DataFrame = {
     val contentIds: List[String] = recordTime(report.select(col("content_id")).distinct().collect().map(_ (0)).toList.asInstanceOf[List[String]], "Time taken to get the content IDs- ")
+    JobLogger.log("ContentIds are" + contentIds, None, INFO)
     val contentMetaDataDF = ESUtil.getAssessmentNames(spark, contentIds, AppConf.getConfig("assessment.metrics.content.index"), AppConf.getConfig("assessment.metrics.supported.contenttype"))
     println("contentMetaDataDF" + contentMetaDataDF.show(false))
     report.join(contentMetaDataDF, report.col("content_id") === contentMetaDataDF.col("identifier"), "left_outer") // Doing right join since to generate report only for the "SelfAssess" content types
