8a7e34bd526cbfe2b8a8c0367b0dcada1a7e11a6
Manjunath Davanam <manjunathd@ilimi.in>
2020-05-22 20:32:31 +0530

    Issue #000 fix: assessment table

--- data-products/src/main/scala/org/sunbird/analytics/job/report/AssessmentMetricsJob.scala
+++ data-products/src/main/scala/org/sunbird/analytics/job/report/AssessmentMetricsJob.scala
@@ -235,16 +235,20 @@ object AssessmentMetricsJob extends optional.Application with IJob with BaseRepo
    * @return - Assessment denormalised dataframe
    */
   def denormAssessment(report: DataFrame)(implicit spark: SparkSession): DataFrame = {
-    val contentIds: List[String] = recordTime(report.select(col("content_id")).distinct().collect().map(_ (0)).toList.asInstanceOf[List[String]], "Time taken to get the content IDs- ")
-    JobLogger.log("ContentIds are" + contentIds, None, INFO)
-   // val contentMetaDataDF = ESUtil.getAssessmentNames(spark, contentIds, AppConf.getConfig("assessment.metrics.content.index"), AppConf.getConfig("assessment.metrics.supported.contenttype"))
-   // println("contentMetaDataDF" + contentMetaDataDF.show(false))
+    println("reportDFSHow" + report.show(false))
+    val contentIds = List("do_31296982713786368012","do_31302079352361779219")
+    //val contentIds: List[String] = recordTime(report.select(col("content_id")).distinct().collect().map(_ (0)).toList.asInstanceOf[List[String]], "Time taken to get the content IDs- ")
+    //JobLogger.log("ContentIds are" + contentIds, None, INFO)
+    val testDF = ESUtil.getAssessmentNames(spark, contentIds, AppConf.getConfig("assessment.metrics.content.index"), AppConf.getConfig("assessment.metrics.supported.contenttype"))
+    println("ES-API" +testDF.show(false))
+    // println("contentMetaDataDF" + contentMetaDataDF.show(false))
     //report.join(contentMetaDataDF, report.col("content_id") === contentMetaDataDF.col("identifier"), "left_outer") // Doing right join since to generate report only for the "SelfAssess" content types
     import spark.implicits._
     val contentMetaDataDF = Seq(
       ("do_31296982713786368012", "bat"),
       ("do_31302079352361779219", "mouse")
     ).toDF("identifier", "name")
+
     report.join(contentMetaDataDF, report.col("content_id") === contentMetaDataDF.col("identifier"), "left_outer") // Doing right join since to generate report only for the "SelfAssess" content types
       .select(
         col("name").as("content_name"),
