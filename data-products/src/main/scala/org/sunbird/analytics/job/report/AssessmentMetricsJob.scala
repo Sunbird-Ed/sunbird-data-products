f4b05ea88c3db1cd1e4062ba797045542e06b9e6
Isha Wakankar <iwakankar@hotmail.com>
2020-07-07 08:23:02 +0530

    .

--- data-products/src/main/scala/org/sunbird/analytics/job/report/AssessmentMetricsJob.scala
+++ data-products/src/main/scala/org/sunbird/analytics/job/report/AssessmentMetricsJob.scala
@@ -136,26 +136,14 @@ object AssessmentMetricsJob extends optional.Application with IJob with BaseRepo
   *here, it will add some more user details
   * */
 
-    val usDf = userCourseDenormDF.withColumn("firstname",lit("TestUser"))
-      .withColumn("lastname",lit("Name"))
-      .withColumn("maskedemail",lit("**@gmail.com"))
-      .withColumn("maskedphone",lit("***"))
-      .withColumn("districtname",lit("LL"))
-      .withColumn("externalid",lit("087654345678"))
-      .withColumn("schoolname",lit("School-x"))
-      .withColumn("schooludisecode",lit("20"))
-      .withColumn("statename",lit("KA"))
-      .withColumn("orgname",lit("TestOrg"))
-      .withColumn("username",concat_ws(" ", col("firstname"), col("lastname")))
-
     val userDenormDF = userCourseDenormDF
-      .join(usDf, userDF.col("userid") === userCourseDenormDF.col("userid"), "inner")
+      .join(userDF, userDF.col("userid") === userCourseDenormDF.col("userid"), "inner")
       .select(
         userCourseDenormDF.col("courseid"),
         userCourseDenormDF.col("batchid"),
         userCourseDenormDF.col("active"),
         userCourseDenormDF.col("course_channel"),
-        usDf.col("*"))
+        userDF.col("*"))
 
     val assessmentDF = getAssessmentData(assessmentProfileDF)
     /**
