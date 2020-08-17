020ac5805dd87d8036fe108a0a616ebd0315906f
Hari-stackroute <40484996+Hari-stackroute@users.noreply.github.com>
2020-08-17 19:15:26 +0530

    SC-1833 Modify userdeclaration report generation (#190)
    * SC-1833 Draft changes of self-declared user report
    
    * SC-1833 modifying userdeclaration report generation according to new table user_declarations and adding error, status columns to report
    
    * SC-1833 added zip creation logic for person csv's
    
    * SC-1833 added zip creation logic on persona's in phython script
    
    * SC-1833 removed the wrong code
    
    * SC-1833 added test case

---
+++ python-scripts/src/main/python/dataproducts/services/location/user_declaration_report.py
@@ -0,0 +1,45 @@
+"""
+Persona wise segragation of User Self-declaration detail report
+"""
+import json
+import sys, time
+import os
+import pandas as pd
+import requests
+import pdb
+import shutil
+
+from datetime import date, datetime
+from pathlib import Path
+from string import Template
+
+from dataproducts.util.utils import create_json, get_data_from_blob, \
+                            post_data_to_blob, push_metric_event
+
+class UserDeclarationReport:
+    def __init__(self, data_store_location, states):
+        self.data_store_location = Path(data_store_location)
+        self.states = states
+
+
+    def init(self):
+        result_loc = self.data_store_location.joinpath('location')
+        for slug in self.states.split(","):
+            slug = slug.strip()
+            state_result_loc = result_loc.joinpath(slug)
+            os.makedirs(state_result_loc, exist_ok=True)
+            try:
+                get_data_from_blob(state_result_loc.joinpath('declared_user_detail', '{}.csv'.format(slug)))
+            except Exception as e:
+                print("declared_user_detail not available for "+slug)
+                continue
+                
+            user_df = pd.read_csv(state_result_loc.joinpath('declared_user_detail', '{}.csv'.format(slug)))
+            os.makedirs(state_result_loc.joinpath('personas'), exist_ok=True)
+            for persona, user_data in user_df.groupby('Persona'):
+                user_data.to_csv(state_result_loc.joinpath('personas', persona.lower()+".csv"), index=False)
+
+            shutil.make_archive(str(state_result_loc.joinpath('declared_user_detail', slug)),
+                                'zip',
+                                str(state_result_loc.joinpath('personas')))
+            post_data_to_blob(state_result_loc.joinpath('declared_user_detail', '{}.zip'.format(slug)))
