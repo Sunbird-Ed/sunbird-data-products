0f1a726e4f2513a152ac98abcc576e4b6042c8bf
Harikumar Palemkota <harip@ilimi.in>
2020-09-01 23:16:14 +0530

    SC-1999 draft version of migrating group service data migration

--- etl-jobs/src/main/scala/org/sunbird/learner/jobs/GroupsServiceTablesMigration.scala
+++ etl-jobs/src/main/scala/org/sunbird/learner/jobs/GroupsServiceTablesMigration.scala
@@ -1,4 +1,4 @@
-
+package org.sunbird.learner.jobs
 
 import org.apache.spark.sql.{Encoders, SaveMode, SparkSession}
 import org.apache.spark.storage.StorageLevel
@@ -31,34 +31,34 @@ object GroupsServiceTablesMigration extends Serializable {
         val groupRes = time(migrateGroup());
         Console.println("Time taken to execute migrateGroupsData script", groupRes._1);
         
-       /* val userGroupRes = time(migrateUserGroup());
+        val userGroupRes = time(migrateUserGroup());
         Console.println("Time taken to execute migrateGroupsData script", userGroupRes._1);
         
-        val res = time(migrateGroupMember());
-        Console.println("Time taken to execute migrateGroupsData script", res._1);*/
+        val groupMemberRes = time(migrateGroupMember());
+        Console.println("Time taken to execute migrateGroupsData script", groupMemberRes._1);
     }
     
     def migrateGroup() (implicit spark: SparkSession): Unit = {
         val groupSchema = Encoders.product[Group].schema
         val sunbirdGroup = spark.read.format("org.apache.spark.sql.cassandra").schema(groupSchema).option("keyspace", "sunbird").option("table", "group").load().persist(StorageLevel.MEMORY_ONLY)
-        println("user_org data Count : " + sunbirdGroup.count())
-        sunbirdGroup.write.format("org.apache.spark.sql.cassandra").option("keyspace", "stagingsunbird").option("table", "group").mode(SaveMode.Append).save()
+        println("group data Count : " + sunbirdGroup.count())
+        sunbirdGroup.write.format("org.apache.spark.sql.cassandra").option("keyspace", "sunbird_groups").option("table", "group").mode(SaveMode.Append).save()
         println("Group count post migration: " + sunbirdGroup.count())
     }
     
     def migrateUserGroup() (implicit spark: SparkSession): Unit = {
         val userGroupSchema = Encoders.product[UserGroup].schema
         val sunbirdUserGroup = spark.read.format("org.apache.spark.sql.cassandra").schema(userGroupSchema).option("keyspace", "sunbird").option("table", "user_group").load().persist(StorageLevel.MEMORY_ONLY)
-        println("user_org data Count : " + sunbirdUserGroup.count())
-        sunbirdUserGroup.write.format("org.apache.spark.sql.cassandra").option("keyspace", "stagingsunbird").option("table", "user_group").mode(SaveMode.Append).save()
+        println("user_group data Count : " + sunbirdUserGroup.count())
+        sunbirdUserGroup.write.format("org.apache.spark.sql.cassandra").option("keyspace", "sunbird_groups").option("table", "user_group").mode(SaveMode.Append).save()
         println("User-group count post migration: " + sunbirdUserGroup.count())
     }
     
     def migrateGroupMember() (implicit spark: SparkSession): Unit = {
         val groupMemberSchema = Encoders.product[UserGroup].schema
         val sunbirdGroupMember = spark.read.format("org.apache.spark.sql.cassandra").schema(groupMemberSchema).option("keyspace", "sunbird").option("table", "group_member").load().persist(StorageLevel.MEMORY_ONLY)
-        println("user_org data Count : " + sunbirdGroupMember.count())
-        sunbirdGroupMember.write.format("org.apache.spark.sql.cassandra").option("keyspace", "stagingsunbird").option("table", "group_member").mode(SaveMode.Append).save()
+        println("group_member data Count : " + sunbirdGroupMember.count())
+        sunbirdGroupMember.write.format("org.apache.spark.sql.cassandra").option("keyspace", "sunbird_groups").option("table", "group_member").mode(SaveMode.Append).save()
         println("Group-member count post migration: " + sunbirdGroupMember.count())
     }
     
