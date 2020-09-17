abca22dbd76d4a5408c60c02d63f5c90ec513368
Utkarsha Kapoor <utkit14@gmail.com>
2020-09-17 11:37:04 +0530

    Question response exhaust

--- data-products/src/main/scala/org/sunbird/analytics/job/report/QuestionResponseExhaust.scala
+++ data-products/src/main/scala/org/sunbird/analytics/job/report/QuestionResponseExhaust.scala
@@ -1,11 +1,16 @@
 package org.sunbird.analytics.job.report
 
+import java.util.concurrent.atomic.AtomicInteger
+
 import org.apache.spark.SparkContext
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.StructType
-import org.apache.spark.sql.{DataFrame, Encoders, SparkSession}
-import org.ekstep.analytics.framework.util.{CommonUtil, JSONUtils, JobLogger}
+import org.apache.spark.sql.{DataFrame, Encoders, SQLContext, SparkSession}
+import org.ekstep.analytics.framework.Level._
+import org.ekstep.analytics.framework.util.DatasetUtil.extensions
+import org.ekstep.analytics.framework.util.{CommonUtil, JSONUtils, JobLogger, RestUtil}
 import org.ekstep.analytics.framework.{FrameworkContext, JobConfig, JobContext, StorageConfig}
-import org.sunbird.analytics.job.report.AssessmentMetricsJobV2.cassandraUrl
+import org.sunbird.analytics.util.Constants
 import org.sunbird.cloud.storage.conf.AppConf
 
 import scala.collection.mutable
@@ -13,9 +18,15 @@ import scala.collection.mutable
 case class AssessmentAggData(course_id: String, batch_id: String, user_id: String, content_id: String, attempt_id: String,
                              created_on: String, grand_total: String, last_attempted_on: String, question: List[QuestionData],
                              total_max_score: String, total_score: String, updated_on: String)
-case class QuestionData(id: String, assess_ts: String, max_score: String, score: Double, `type`: String,
+case class QuestionData(id: String, max_score: Double, score: Double, `type`: String,
                         title: String, resvalues: List[Map[String, String]], params: List[Map[String, String]],
                         description: String, duration: String)
+case class CourseResponse(result: CourseResult, responseCode: String)
+case class CourseResult(count: Int, content: List[CourseBatchInfo])
+case class CourseBatchInfo(identifier: String, name: String, batches: List[BatchInfo])
+case class BatchInfo(batchId: String, name: String)
+case class CourseBatchName(courseId: String, batchId: String)
+
 
 object QuestionResponseExhaust extends optional.Application with BaseReportsJob {
 
@@ -24,6 +35,8 @@ object QuestionResponseExhaust extends optional.Application with BaseReportsJob 
   val metrics: mutable.Map[String, BigInt] = mutable.Map[String, BigInt]()
   val baseCourseReport = new BaseCourseReport
   def name(): String = "CourseMetricsJobV2"
+  val cassandraUrl = "org.apache.spark.sql.cassandra"
+
 
   def main(config: String)(implicit sc: Option[SparkContext] = None, fc: Option[FrameworkContext] = None) {
     JobLogger.init("CourseMetricsJob")
@@ -60,15 +73,95 @@ object QuestionResponseExhaust extends optional.Application with BaseReportsJob 
   }
 
   def prepareReport(spark: SparkSession, storageConfig: StorageConfig, loadData: (SparkSession, Map[String, String], String, StructType) => DataFrame,
-                    config: JobConfig, batchIds: List[String]): Unit = {
+                    config: JobConfig, batchIds: List[String])(implicit fc: FrameworkContext): Unit = {
+    implicit val sparkSession: SparkSession = spark
+
+    implicit val sqlContext: SQLContext = spark.sqlContext
+    import sqlContext.implicits._
+
+    val modelParams = config.modelParams.get
+    val reportPath = modelParams.getOrElse("reportPath","question-exhaust-reports/").asInstanceOf[String]
+
+    val questionReportDF = getQuestionReport(spark, loadData)
+    val courseBatchName = getCourseBatchName(JSONUtils.serialize(config.modelParams.get("esConfig"))).toDF()
+      .withColumn("batchInfo", explode(col("batches")))
+      .withColumn("batchId", col("batchInfo.batchId"))
+      .withColumn("batchName", col("batchInfo.name"))
+      .withColumnRenamed("identifier", "courseId")
+      .withColumnRenamed("name", "courseName")
+      .drop("batchInfo", "batches")
+    val courseBatchList = courseBatchName.collect()
+
+    val batchCount = new AtomicInteger(courseBatchList.length)
+
+    for (index <- courseBatchList.indices) {
+    val row = courseBatchList(index)
+      val reportDF = getReportDF(courseBatchName, questionReportDF, row.getString(2))
+      val totalRecords = reportDF.count()
+      if (totalRecords > 0) {
+        saveReportToBlobStore(row.getString(2), reportDF, storageConfig, totalRecords, reportPath)
+      }
+    }
+  }
+
+  def getReportDF(courseBatchInfo: DataFrame, questionDF: DataFrame, batchId: String) = {
+    JobLogger.log("Creating report for batch " + batchId, None, INFO)
+
+    val reportDF = questionDF.where(col("batch_id") === batchId)
+      .join(courseBatchInfo, courseBatchInfo.col("batchId") === questionDF.col("batch_id"), "inner")
+      .select(
+        col("course_id").as("Collection ID"),
+        col("courseName").as("Course Name"),
+        col("batch_id").as("Batch ID"),
+        col("batchName").as("Batch Name"),
+        col("user_id").as("DIKSHA UUID"),
+        col("attempt_id").as("Attempt Id"),
+        col("last_attempted_on").as("Attempted On"),
+        col("questionid").as("Question ID"),
+        col("questiontype").as("Question Type"),
+        col("questiontitle").as("Question Title"),
+        col("questiondescription").as("Question Description"),
+        col("questionduration").as("Question Duration"),
+        col("questionscore").as("Question Score"),
+        col("questionmaxscore").as("Question Max Score"),
+        col("questionoption").as("Question Options"),
+        col("questionresponse").as("Question Response")
+      )
+    reportDF
+  }
+
+  def getCourseBatchName(query: String): List[CourseBatchInfo] = {
+    val apiUrl = Constants.COMPOSITE_SEARCH_URL
+    val response = RestUtil.post[CourseResponse](apiUrl, query)
+    if (null != response && response.responseCode.equalsIgnoreCase("ok")
+              && null != response.result.content && response.result.content.nonEmpty) {
+      response.result.content
+    } else List[CourseBatchInfo]()
+  }
+
+  def getQuestionReport(spark: SparkSession, loadData: (SparkSession, Map[String, String], String, StructType) => DataFrame): DataFrame = {
     val assessmentSchema = Encoders.product[AssessmentAggData].schema
     val assessmentProfileDF = loadData(spark, Map("table" -> "assessment_aggregator", "keyspace" -> sunbirdCoursesKeyspace), cassandraUrl, assessmentSchema)
-    println("assessmentDF")
-    assessmentProfileDF.show()
-    val data = assessmentProfileDF.rdd.map{f =>
-      println("assessment: " + f)
-    }
 
+    val questionDF = assessmentProfileDF
+      .withColumn("questiondata",explode_outer(col("question")) )
+      .withColumn("questionid" , col("questiondata.id"))
+      .withColumn("questiontype", col("questiondata.type"))
+      .withColumn("questiontitle", col("questiondata.title"))
+      .withColumn("questiondescription", col("questiondata.description"))
+      .withColumn("questionduration", col("questiondata.duration"))
+      .withColumn("questionscore", col("questiondata.score"))
+      .withColumn("questionmaxscore", col("questiondata.max_score"))
+      .withColumn("questionresponse", col("questiondata.resvalues"))
+      .withColumn("questionoption", col("questiondata.params"))
+      .drop("question", "questiondata")
+    questionDF
+  }
+
+  def saveReportToBlobStore(batchid: String, reportDF: DataFrame, storageConfig: StorageConfig, totalRecords: Long, reportPath: String): Unit = {
+    reportDF
+      .saveToBlobStore(storageConfig, "csv", reportPath + "report-" + batchid, Option(Map("header" -> "true")), None)
+    JobLogger.log(s"CourseMetricsJob: records stats before cloud upload: { batchId: ${batchid}, totalNoOfRecords: $totalRecords }} ", None, INFO)
   }
 
 
--- data-products/src/test/scala/org/sunbird/analytics/job/report/TestQuestionResponseExhaust.scala
+++ data-products/src/test/scala/org/sunbird/analytics/job/report/TestQuestionResponseExhaust.scala
@@ -1,6 +1,6 @@
 package org.sunbird.analytics.job.report
 
-import org.apache.spark.sql.{DataFrame, Encoders, SparkSession}
+import org.apache.spark.sql.{DataFrame, Encoders, SQLContext, SparkSession}
 import org.ekstep.analytics.framework.util.JSONUtils
 import org.ekstep.analytics.framework.{FrameworkContext, JobConfig, StorageConfig}
 import org.scalamock.scalatest.MockFactory
@@ -16,17 +16,26 @@ class TestQuestionResponseExhaust extends BaseReportSpec with MockFactory {
     super.beforeAll()
     spark = getSparkSession();
 
-    assessmentProfileDF = spark
-      .read
-      .format("com.databricks.spark.csv")
-      .option("header", "true")
-      .load("src/test/resources/question-report/assessment_agg_data.csv")
-      .cache()
+//    assessmentProfileDF = spark
+//      .read
+//      .format("com.databricks.spark.csv")
+//      .option("header", "true")
+//      .load("src/test/resources/question-report/assessment_agg_data.csv")
+//      .cache()
+
+    implicit val sqlContext: SQLContext = spark.sqlContext
+    import sqlContext.implicits._
+
+    assessmentProfileDF = List(
+      AssessmentAggData("do_213045554296676352193", "0130207040554762244", "04b12c81-67e3-4a1a-8c5d-222d2019ae2d", "do_2130257157776097281995", "8017dc1150022b9370c2effb5be681b2", "2020-06-18 13:26:14.583000+0000", "12.33/13.0", "2020-06-18 12:51:21.324000+0000", List(QuestionData("do_2129194942597447681595", 1, 0, "mcq","choose one",List(Map("2" -> """{"text" -> "Quantity}""")), List(), "choose one", "0.0"), QuestionData("do_2129194957310361601598", 1, 0, "mcq", "choose correct one", List(), List(Map("1" -> """{"text":"Pen Paper Test"}""", "2" -> """{"text":"Online Test"}""")), "", "0.0")), "13", "12.33", "2020-06-18 13:26:14.631000+0000"),
+      AssessmentAggData("do_213045554296676352193", "0130207040554762244", "8454cb21-3ce9-4e30-85b5-fade097880d8", "do_2130257157776097281995", "85b3283314c2680581f9447c0b792dc2a3", "2020-06-19 13:26:14.583000+0000", "2.33/3.0", "2020-06-19 12:51:21.324000+0000", List(QuestionData("do_2129194942597447681595", 1, 1, "mcq","Which three subject areas are mainly assessed by PISA?",List(), List(Map("1" -> """{"text":"Science, Fine Arts and Environments Studies\n"}""", "2" -> """{"text":"Reading Literacy, Mathematical Literacy and Scientific Abilities\n"}""")), "choose one", "0.0"), QuestionData("do_2129194957310361601598", 1, 0, "mcq", "choose correct one", List(), List(Map("1" -> """{"text":"Pen Paper Test"}""", "2" -> """{"text":"Online Test"}""")), "", "0.0")), "13", "12.33", "2020-06-18 13:26:14.631000+0000"),
+      AssessmentAggData("do_11269863149590937614", "0130759417337364480", "8454cb21-3ce9-4e30-85b5-fade097880d8", "do_1126980913198940161169", "85b32814c2680581f9447c0b792dc2a3", "2020-06-19 13:26:14.583000+0000", "0/3", "2020-06-19 12:51:21.324000+0000", List(QuestionData("do_2129194942597447681595", 1, 1, "mcq","Which three subject areas are mainly assessed by PISA?",List(), List(Map("1" -> """{"text":"Science, Fine Arts and Environments Studies\n"}""", "2" -> """{"text":"Reading Literacy, Mathematical Literacy and Scientific Abilities\n"}""")), "", "0.0")), "3", "0", "2020-06-18 13:26:14.631000+0000")
+    ).toDF()
   }
 
   "TestQuestionResponseExhaust" should "generate the csv with all the required fields" in {
     implicit val mockFc = mock[FrameworkContext]
-    val strConfig = """{"search":{"type":"none"},"model":"org.sunbird.analytics.job.report.QuestionResponseExhaust","modelParams":{"batchFilters":["TPD"],"fromDate":"$(date --date yesterday '+%Y-%m-%d')","toDate":"$(date --date yesterday '+%Y-%m-%d')","sparkCassandraConnectionHost":"127.0.0.0","sparkElasticsearchConnectionHost":"'$sunbirdPlatformElasticsearchHost'","sparkRedisConnectionHost":"'$sparkRedisConnectionHost'","sparkUserDbRedisIndex":"4","contentFilters":{"request":{"filters":{"framework":"TPD"},"sort_by":{"createdOn":"desc"},"limit":10000,"fields":["framework","identifier","name","channel"]}},"reportPath":"question-reports/"},"output":[{"to":"console","params":{"printEvent":false}}],"parallelization":8,"appName":"Question Response Dashboard Metrics","deviceMapping":false}"""
+    val strConfig = """{"search":{"type":"none"},"model":"org.sunbird.analytics.job.report.QuestionResponseExhaust","modelParams":{"batchFilters":["TPD"], "esConfig": {"request":{"filters":{"objectType":["Content"],"contentType":["Course"],"identifier":[]},"fields":["identifier","name","batches.batchId","batches.name"],"limit":10000}},"fromDate":"$(date --date yesterday '+%Y-%m-%d')","toDate":"$(date --date yesterday '+%Y-%m-%d')","sparkCassandraConnectionHost":"127.0.0.0","sparkElasticsearchConnectionHost":"'$sunbirdPlatformElasticsearchHost'","sparkRedisConnectionHost":"'$sparkRedisConnectionHost'","sparkUserDbRedisIndex":"4","contentFilters":{"request":{"filters":{"framework":"TPD"},"sort_by":{"createdOn":"desc"},"limit":10000,"fields":["framework","identifier","name","channel"]}},"reportPath":"question-exhaust-reports/"},"output":[{"to":"console","params":{"printEvent":false}}],"parallelization":8,"appName":"Question Response Dashboard Metrics","deviceMapping":false}"""
     val config = JSONUtils.deserialize[JobConfig](strConfig)
 
     val outputLocation = "/tmp/question-metrics"
