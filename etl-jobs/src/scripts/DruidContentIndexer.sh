2ea0ad000a57d8e5cad1f9549a868e9ab9d95373
Isha Wakankar <iwakankar@hotmail.com>
2020-09-17 22:00:53 +0530

    Indexing vdn content into druid datasource

--- etl-jobs/src/scripts/DruidContentIndexer.sh
+++ etl-jobs/src/scripts/DruidContentIndexer.sh
@@ -3,11 +3,13 @@ set -e
 # Configurations
 druidCoordinatorIP=""
 dataSourceName="content-model-snapshot"
+vdnDataSourceName="vdn-content-model-snapshot"
 today=`date +%Y-%m-%d`
 interval="2019-01-01_$today"
 now=`date +%Y-%m-%d-%s`
 home=`echo $HOME`
 ingestionSpecFilePath="${home}/etl-jobs-1.0/druid_models/content_index_batch.json"
+vdnIngestionSpecFilePath="${home}/etl-jobs-1.0/druid_models/vdn_content_index_batch.json"
 jobJarPath="${home}/etl-jobs-1.0/etl-jobs-1.0.jar"
 jobConfPath="${home}/etl-jobs-1.0/resources/ESCloudUploader.conf"
 
@@ -46,3 +48,36 @@ curl -X 'DELETE' -H 'Content-Type:application/json' http://${druidCoordinatorIP}
 printf "\n>>> success!\n"
 
 
+# Indexing into VDN druid datasource
+
+bkpIFS="$IFS"
+vdnsegmentIds=$(curl -X 'GET' -H 'Content-Type:application/json' http://${druidCoordinatorIP}:8081/druid/coordinator/v1/metadata/datasources/${vdnDataSourceName}/segments)
+IFS=',]['
+read -r -a arrayData <<< ${vdnsegmentIds}
+IFS="$bkpIFS"
+
+# start the spark script to fetch Elasticsearch data and write it to a file and upload to cloud
+spark/bin/spark-submit \
+--conf spark.driver.extraJavaOptions="-Dconfig.file=${jobConfPath}" \
+--class org.ekstep.analytics.jobs.ESCloudUploader \
+${jobJarPath}
+
+printf "\n>>> submit ingestion task to Druid!\n"
+
+# submit task to start batch ingestion
+curl -X 'POST' -H 'Content-Type:application/json' -d @${vdnIngestionSpecFilePath} http://${druidCoordinatorIP}:8090/druid/indexer/v1/task
+
+for index in "${!arrayData[@]}"
+do
+    val=( $(eval echo ${arrayData[index]}) )
+    printf "\n>>> Disabling segment id: $val \n"
+    # disable older segments
+    curl -X 'DELETE' -H 'Content-Type:application/json' http://${druidCoordinatorIP}:8081/druid/coordinator/v1/datasources/${vdnDataSourceName}/segments/$val
+done
+
+printf "\n>>> Deleting segments from interval $interval \n"
+
+# delete older segments
+curl -X 'DELETE' -H 'Content-Type:application/json' http://${druidCoordinatorIP}:8081/druid/coordinator/v1/datasources/${vdnDataSourceName}/intervals/${interval}
+
+printf "\n>>> success!\n"
