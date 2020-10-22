0e64a51f49a7a282dd7ea3016396e9d7c11eefa1
Manjunath Davanam <manjunathd@ilimi.in>
2020-10-22 13:41:25 +0530

    Issue SB-20903 fix: Collection Summary Report job - Testcase improvement

--- data-products/src/test/scala/org/sunbird/analytics/job/report/TestCourseEnrollmentJob.scala
+++ data-products/src/test/scala/org/sunbird/analytics/job/report/TestCourseEnrollmentJob.scala
@@ -1,27 +1,109 @@
 package org.sunbird.analytics.job.report
 
-import org.ekstep.analytics.framework.Dispatcher
-import org.ekstep.analytics.framework.DruidQueryModel
-import org.ekstep.analytics.framework.Fetcher
-import org.ekstep.analytics.framework.JobConfig
+import org.apache.spark.sql.functions.{udf, _}
+import org.apache.spark.sql.types.{ArrayType, MapType, StringType, StructType}
+import org.apache.spark.sql.{DataFrame, Encoders, SparkSession}
 import org.ekstep.analytics.framework.util.JSONUtils
-import org.ekstep.analytics.model.Metrics
-import org.ekstep.analytics.model.OutputConfig
-import org.ekstep.analytics.model.QueryDateRange
-import org.ekstep.analytics.model.ReportConfig
-import org.sunbird.analytics.util.SparkSpec;
+import org.ekstep.analytics.framework.{FrameworkContext, JobConfig, StorageConfig}
+import org.scalamock.scalatest.MockFactory
+import org.sunbird.analytics.util.UserData
 
-class TestCourseEnrollmentJob extends SparkSpec(null) {
+import scala.collection.mutable
 
-  "CourseEnrollmentJob" should "execute and won't throw any errors/exception" in {
-    val config = """{"reportConfig": {"id": "tpd_metrics","metrics": [],"labels": {"completionCount": "Completion Count","status": "Status","enrollmentCount": "Enrollment Count","courseName": "Course Name","batchName": "Batch Name"},"output": [{"type": "csv","dims": ["identifier", "channel", "name"],"fileParameters": ["id", "dims"]}]},"esConfig": {"request": {"filters": {"objectType": ["Content"],"contentType": ["Course"],"identifier": [],"status": ["Live"]},"limit": 10000}},"key": "druid-reports/","filePath": "src/test/resources/","bucket": "test-container","folderPrefix": ["slug", "reportName"]}"""
-    val jobConfig = JSONUtils.deserialize[JobConfig](config)
 
-    val reportConfig = ReportConfig("tpd_metrics", "", QueryDateRange(None, Option(""), Option("")), List(Metrics("", "", DruidQueryModel("", "", "", Option("")))), Map("date" -> "Date", "completionCount" -> "Completion Count", "status" -> "Status", "enrollmentCount" -> "Enrollment Count", "courseName" -> "Course Name", "batchName" -> "Batch Name"), List(OutputConfig("csv", None, List("identifier", "channel", "name"), List("id", "dims"))))
-    val esConfig = Map("request" -> Map("filters" -> Map("objectType" -> List("Content"), "contentType" -> List("Course"), "identifier" -> List(), "status" -> List("Live"))))
-    val modelParams = Map("esConfig" -> esConfig, "reportConfig" -> reportConfig, "bucket" -> "test-container", "key" -> "druid-reports/", "filePath" -> "src/test/resources/", "courseStatus" -> List("Live"), "courseIds" -> List("do_1126981011606323201176", "do_112470675618004992181"))
-    val con = JobConfig(Fetcher("none", None, None, None), None, None, "org.ekstep.analytics.model.TPDEnrollmentMetricsModel", Option(modelParams), Option(Array(Dispatcher("console", Map("printEvent" -> false.asInstanceOf[AnyRef])))), Option(8), Option("TPD Course Consumption Metrics Model"), Option(false), None, None)
+/** *
+ * *************************************** START OF TEST INPUT ****************************************
+ * Report1 = (batch = (0130320389509939204), course = (do_112636984058314752121), enrolledUsers(c7ef3848-bbdb-4219-8344-817d5b8103fa, user021), channel (b00bc992ef25f1a9a8d63291e20efc8d)
+ * Report2 = (batch = (0130293763489873929), course = (do_1130293726460805121168), enrolledUsers(f3dd58a4-a56f-4c1d-95cf-3231927a28e9, user026), channel (013016492159606784174))
+ * Report3 = (batch = (01303150537737011211), course = (do_1130314965721088001129), enrolledUsers(user026, user025), channel (b00bc992ef25f1a9a8d63291e20efc8d))
+ * Report4 = (batch = (0130271096968396800), course = (do_1130264512015646721166), enrolledUsers(user027), channel (013016492159606784174))
+ *
+ * channel_org_map = (channel = b00bc992ef25f1a9a8d63291e20efc8d, orgId = 0126391644091351040 , orgName = "MPPS BAYYARAM"), (channel = 013016492159606784174, orgId = 0125302909498654720 , orgName = MPPS SIMHACHALNAGAR)
+ * user_org_map = (
+ * (orgName = "MPPS BAYYARAM", userId = (c7ef3848-bbdb-4219-8344-817d5b8103fa, user021, f3dd58a4-a56f-4c1d-95cf-3231927a28e9)),
+ * orgName = "MPPS SIMHACHALNAGAR", userId = (user026, user025, user027)),
+ * )
+ * *************************************** END OF TEST INPUT ******************************************
+ */
 
-    CourseEnrollmentJob.main(JSONUtils.serialize(con))(Option(sc))
+
+class TestCollectionSummaryJob extends BaseReportSpec with MockFactory {
+
+
+  var spark: SparkSession = _
+
+  var courseBatchDF: DataFrame = _
+  var userEnrolments: DataFrame = _
+  var userDF: DataFrame = _
+  var organisationDF: DataFrame = _
+  var reporterMock: BaseReportsJob = mock[BaseReportsJob]
+  val sunbirdCoursesKeyspace = "sunbird_courses"
+  val sunbirdKeyspace = "sunbird"
+  val esIndexName = "composite"
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    spark = getSparkSession();
+    courseBatchDF = spark
+      .read
+      .format("com.databricks.spark.csv")
+      .option("header", "true")
+      .load("src/test/resources/collection-summary/modified/course_batch_data.csv")
+      .cache()
+
+    userEnrolments = spark
+      .read
+      .format("com.databricks.spark.csv")
+      .option("header", "true")
+      .load("src/test/resources/collection-summary/modified/user_courses_data.csv")
+      .cache()
+
+    organisationDF = spark
+      .read
+      .format("com.databricks.spark.csv")
+      .option("header", "true")
+      .load("src/test/resources/collection-summary/modified/orgTable.csv")
+      .cache()
+
+    userDF = spark.read.json("src/test/resources/collection-summary/modified/user_data.json")
+      .cache()
+
   }
+
+  val convertMethod = udf((value: mutable.WrappedArray[String]) => {
+    if (null != value && value.nonEmpty)
+      value.toList.map(str => JSONUtils.deserialize(str)(manifest[Map[String, String]])).toArray
+    else null
+  }, new ArrayType(MapType(StringType, StringType), true))
+
+  it should "generate the report for all the batches" in {
+
+    (reporterMock.fetchData _)
+      .expects(spark, Map("table" -> "course_batch", "keyspace" -> sunbirdCoursesKeyspace, "cluster" -> "LMSCluster"), "org.apache.spark.sql.cassandra", new StructType())
+      .returning(courseBatchDF)
+
+    (reporterMock.fetchData _)
+      .expects(spark, Map("table" -> "user_enrolments", "keyspace" -> sunbirdCoursesKeyspace, "cluster" -> "LMSCluster"), "org.apache.spark.sql.cassandra", new StructType())
+      .returning(userEnrolments.withColumn("certificates", convertMethod(split(userEnrolments.col("certificates"), ",").cast("array<string>"))))
+      .anyNumberOfTimes()
+
+    (reporterMock.fetchData _)
+      .expects(spark, Map("table" -> "organisation", "keyspace" -> sunbirdKeyspace, "cluster" -> "LMSCluster"), "org.apache.spark.sql.cassandra", new StructType())
+      .returning(organisationDF)
+      .anyNumberOfTimes()
+
+    val schema = Encoders.product[UserData].schema
+    (reporterMock.fetchData _)
+      .expects(spark, Map("table" -> "user", "infer.schema" -> "true", "key.column" -> "userid"), "org.apache.spark.sql.redis", schema)
+      .anyNumberOfTimes()
+      .returning(userDF)
+
+
+    implicit val mockFc: FrameworkContext = mock[FrameworkContext]
+    val strConfig = """{"search": {"type": "none"},"model": "org.sunbird.analytics.job.report.CourseMetricsJob","modelParams": {"batchFilters": ["TPD"],"fromDate": "$(date --date yesterday '+%Y-%m-%d')","toDate": "$(date --date yesterday '+%Y-%m-%d')","sparkCassandraConnectionHost": "127.0.0.0","sparkElasticsearchConnectionHost": "'$sunbirdPlatformElasticsearchHost'","sparkRedisConnectionHost": "'$sparkRedisConnectionHost'","sparkUserDbRedisIndex": "4","contentFilters": {"request": {"filters": {"framework": "TPD"},"sort_by": {"createdOn": "desc"},"limit": 10000,"fields": ["framework", "identifier", "name", "channel"]}},"reportPath": "course-reports/"},"output": [{"to": "console","params": {"printEvent": false}}],"parallelization": 8,"appName": "Course Dashboard Metrics","deviceMapping": false}""".stripMargin
+    implicit val jobConfig = JSONUtils.deserialize[JobConfig](strConfig)
+    val storageConfig = StorageConfig("local", "", "/tmp/course-metrics")
+    CollectionSummaryJob.prepareReport(spark, reporterMock.fetchData, List())
+  }
+
 }
