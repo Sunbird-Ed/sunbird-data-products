9b7fd1a1ee23dd2c37b11eeb29da971e38942c65
Isha Wakankar <iwakankar@hotmail.com>
2020-12-09 21:00:01 +0530

    Fixes for FunnelReport test case issue

--- data-products/src/test/scala/org/sunbird/analytics/sourcing/TestFunnnelReport.scala
+++ data-products/src/test/scala/org/sunbird/analytics/sourcing/TestFunnnelReport.scala
@@ -1,13 +1,14 @@
 package org.sunbird.analytics.sourcing
 
 import java.time.{ZoneOffset, ZonedDateTime}
+
 import cats.syntax.either._
 import ing.wbaa.druid.client.DruidClient
 import ing.wbaa.druid._
 import io.circe.Json
 import io.circe.parser.parse
 import org.apache.spark.sql.SparkSession
-import org.ekstep.analytics.framework.FrameworkContext
+import org.ekstep.analytics.framework.{FrameworkContext, JobConfig}
 import org.ekstep.analytics.framework.util.JSONUtils
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.Matchers
@@ -46,7 +47,10 @@ class TestFunnnelReport extends SparkSpec with Matchers with MockFactory {
 
     val config = """{"search": {"type": "none"},"model": "org.ekstep.analytics.job.report.FunnelReport","druidConfig": {"queryType": "timeseries","dataSource": "telemetry-events-syncts","intervals": "startdate/enddate","aggregations": [{"name": "visitors","type": "count","fieldName": "actor_id"}],"filters": [{"type": "equals","dimension": "context_cdata_id","value": "program_id"}, {"type": "equals","dimension": "context_cdata_type","value": "Program"}, {"type": "equals","dimension": "context_pdata_id","value": "'$producerEnv'.portal"}],"postAggregation": [],"descending": "false","limitSpec": {"type": "default","limit": 1000000,"columns": [{"dimension": "count","direction": "descending"}]}},"modelParams": {"reportConfig": {"id": "funnel_report","metrics": [],"labels": {"reportDate": "Report Generation Date","name": "Project Name"},"output": [],"outputs": [{"type": "csv","dims": ["identifier", "channel", "name"],"fileParameters": ["id", "dims"]}, {"type": "json","dims": ["identifier", "channel", "name"],"fileParameters": ["id", "dims"]}]},"store": "local","format": "csv","key": "druid-reports/","filePath": "druid-reports/","container": "test-container","folderPrefix": ["slug", "reportName"]},"output": [{"to": "console","params": {"printEvent": false}}],"parallelization": 8,"appName": "Funnel Metrics Report","deviceMapping": false}""".stripMargin
     val configMap = JSONUtils.deserialize[Map[String,AnyRef]](config)
-    FunnelReport.generateFunnelReport(spark, configMap)
+    implicit val jobConfig = JSONUtils.deserialize[JobConfig](config)
+    implicit val sparkCon = spark
+    val funnelReportCount = FunnelReport.execute()
+    funnelReportCount.getOrElse("funnelReportCount",0) should be 2
   }
 
   it should "return 0 if no resources found for correction pending" in {
