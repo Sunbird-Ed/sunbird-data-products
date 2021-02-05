55fe64a9e42b7af598898b22378260fedf2687b5
SowmyaDixit <sowmyadixit7@gmail.com>
2021-02-05 15:32:53 +0530

    Issue #0000 feat: Revert - Changes to enable parallel processing of exhaust job requests

--- data-products/src/main/scala/org/sunbird/analytics/exhaust/OnDemandExhaustJob.scala
+++ data-products/src/main/scala/org/sunbird/analytics/exhaust/OnDemandExhaustJob.scala
@@ -59,6 +59,7 @@ trait OnDemandExhaustJob {
     val encoder = Encoders.product[JobRequest]
     val reportConfigsDf = spark.read.jdbc(url, requestsTable, connProperties)
       .where(col("job_id") === jobId && col("iteration") < 3).filter(col("status").isin(jobStatus: _*));
+    
     val requests = reportConfigsDf.withColumn("status", lit("PROCESSING")).as[JobRequest](encoder).collect()
     requests;
   }
