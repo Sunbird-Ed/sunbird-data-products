8a0a42775922cff991b71aabe2adfbf7d33c2e69
Utkarsha Kapoor <utkit14@gmail.com>
2021-02-08 12:25:07 +0530

    Issue #SB-21144: Testcase improved and code fix for kafka dispachter

--- data-products/src/main/scala/org/sunbird/analytics/model/report/AssessmentCorrectionModel.scala
+++ data-products/src/main/scala/org/sunbird/analytics/model/report/AssessmentCorrectionModel.scala
@@ -8,13 +8,16 @@ import org.apache.spark.sql.{DataFrame, SQLContext}
 import org.ekstep.analytics.framework._
 import org.ekstep.analytics.framework.conf.AppConf
 import org.ekstep.analytics.framework.fetcher.DruidDataFetcher
-import org.ekstep.analytics.framework.util.{JSONUtils, JobLogger}
+import org.ekstep.analytics.framework.util.{CommonUtil, JSONUtils, JobLogger}
 import org.sunbird.analytics.exhaust.BaseReportsJob
 import org.sunbird.analytics.job.report.DruidOutput
 
+import scala.collection.JavaConverters._
+import scala.collection.mutable
 case class SelfAssessData(identifier: String, contentType: String)
 case class AssessEvent (contentid: String, attemptId: String, courseid: String, userid: String, assessEvent: String)
-case class AssessOutputEvent(assessmentTs: Long, batchId: String, courseId: String, userId: String, attemptId: String, contentId: String, events: java.util.List[Map[String, AnyRef]]) extends AlgoOutput with Output
+@scala.beans.BeanInfo
+case class AssessOutputEvent(assessmentTs: Long, batchId: String, courseId: String, userId: String, attemptId: String, contentId: String, events: mutable.Buffer[V3Event]) extends Output with AlgoOutput with scala.Product with scala.Serializable
 
 object AssessmentCorrectionModel extends IBatchModelTemplate[String,V3Event,AssessOutputEvent,AssessOutputEvent] with Serializable with BaseReportsJob {
 
@@ -33,6 +36,18 @@ object AssessmentCorrectionModel extends IBatchModelTemplate[String,V3Event,Asse
   }
 
   override def algorithm(events: RDD[V3Event], config: Map[String, AnyRef])(implicit sc: SparkContext, fc: FrameworkContext): RDD[AssessOutputEvent] = {
+    val res = CommonUtil.time(algorithProcess(events,config))
+
+    JobLogger.log(s"Time take to get SelfAssess data: ${res._1}", None, Level.INFO)
+    res._2
+  }
+
+  override def postProcess(events: RDD[AssessOutputEvent], config: Map[String, AnyRef])(implicit sc: SparkContext, fc: FrameworkContext): RDD[AssessOutputEvent] = {
+    JobLogger.log(s"Total output events: ${events.count()}", None, Level.INFO)
+    events
+  }
+
+  def algorithProcess(events: RDD[V3Event], config: Map[String, AnyRef])(implicit sc: SparkContext, fc: FrameworkContext): RDD[AssessOutputEvent] = {
     implicit val sqlContext = new SQLContext(sc)
     val assessEventDF = getAssessEventData(events, config)
     val druidData = loadDruidData(config)
@@ -40,16 +55,11 @@ object AssessmentCorrectionModel extends IBatchModelTemplate[String,V3Event,Asse
     val joinedDF = assessEventDF.join(druidData, assessEventDF.col("contentid") === druidData.col("identifier"), "inner")
       .select(assessEventDF.col("*"))
 
-    val outputData = joinedDF.rdd.map{f =>
+    joinedDF.rdd.map{f =>
       val assessmentTs: Long = System.currentTimeMillis()
-      AssessOutputEvent(assessmentTs, f.getString(4), f.getString(1), f.getString(2), f.getString(3), f.getString(0), f.getList(5) )
+      val eventList = f.getList(5).asInstanceOf[java.util.List[String]].asScala.map(f => JSONUtils.deserialize[V3Event](f))
+      AssessOutputEvent(assessmentTs, f.getString(4), f.getString(1), f.getString(2), f.getString(3), f.getString(0), eventList)
     }
-    outputData
-  }
-
-  override def postProcess(events: RDD[AssessOutputEvent], config: Map[String, AnyRef])(implicit sc: SparkContext, fc: FrameworkContext): RDD[AssessOutputEvent] = {
-    JobLogger.log(s"Total output events: ${events.count()}", None, Level.INFO)
-    events
   }
 
   def loadDruidData(config: Map[String, AnyRef])(implicit sc: SparkContext, sqlContext: SQLContext, fc: FrameworkContext): DataFrame = {
@@ -65,8 +75,8 @@ object AssessmentCorrectionModel extends IBatchModelTemplate[String,V3Event,Asse
     val assessEvent = events.map { f =>
       val userId = if (f.actor.`type`.equals("User")) f.actor.id else "" // UserID
     val cData = f.context.cdata.getOrElse(List())
-      val attemptIdList = cData.filter(f => f.`type`.equalsIgnoreCase("AttemptId")).map(f => f.id)
-      val attemptId = if (!attemptIdList.isEmpty) attemptIdList.head else ""  // AttempId
+    val attemptIdList = cData.filter(f => f.`type`.equalsIgnoreCase("AttemptId")).map(f => f.id)
+    val attemptId = if (!attemptIdList.isEmpty) attemptIdList.head else ""  // AttempId
     val courseId = f.`object`.get.rollup.getOrElse(RollUp("", "", "", "")).l1 // CourseId
     val contentId = f.`object`.get.id // ContentId
     val event = JSONUtils.serialize(f)
@@ -76,7 +86,8 @@ object AssessmentCorrectionModel extends IBatchModelTemplate[String,V3Event,Asse
     //Filter batchIds from config
     val batchIds: List[String] = config.getOrElse("batchId", List()).asInstanceOf[List[String]]
     val userEnrollmentData = getUserEnrollData(batchIds)
-    val df = assessEvent.join(userEnrollmentData, Seq("courseid", "userid"))
+
+    assessEvent.join(userEnrollmentData, Seq("courseid", "userid"))
       .groupBy("contentid", "courseid", "userid", "attemptId", "batchid")
       .agg(collect_list(col("assessEvent")).as("assessEventList"))
       .withColumn("AttemptId_resolved",
@@ -89,19 +100,17 @@ object AssessmentCorrectionModel extends IBatchModelTemplate[String,V3Event,Asse
         col("batchid"),
         col("assessEventList")
       )
-    df
   }
 
   def getUserEnrollData(batchIds: List[String])(implicit sqlContext: SQLContext): DataFrame = {
     implicit val spark = sqlContext.sparkSession
     if(batchIds.size == 0) {
-      JobLogger.log("No batchid provided")
+      JobLogger.log("No batchid provided", None, Level.INFO)
       loadData(userEnrolmentDBSettings,cassandraFormat, new StructType())
         .where(lower(col("active")).equalTo("true") && col("enrolleddate").isNotNull)
         .select("userid", "courseid", "batchid")
     } else {
-      println(s"Filtering for batches: ${batchIds}")
-      JobLogger.log(s"Filtering for batches: ${batchIds}" )
+      JobLogger.log(s"Filtering for batches: ${batchIds}", None, Level.INFO)
       loadData(userEnrolmentDBSettings,cassandraFormat, new StructType())
         .where(lower(col("active")).equalTo("true") && col("enrolleddate").isNotNull)
         .filter(col("batchid").isin(batchIds: _*))
