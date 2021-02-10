f7ae1524070231cc959c9c6a5dcc94567e1bb7fe
Isha Wakankar <iwakankar@hotmail.com>
2021-02-10 13:22:51 +0530

    .

--- data-products/src/main/scala/org/sunbird/analytics/sourcing/ContentDetailsReport.scala
+++ data-products/src/main/scala/org/sunbird/analytics/sourcing/ContentDetailsReport.scala
@@ -94,10 +94,10 @@ object ContentDetailsReport extends optional.Application with IJob with BaseRepo
   def getDruidQuery(query: String, channel: String): DruidQueryModel = {
     val mapQuery = JSONUtils.deserialize[Map[String,AnyRef]](query)
     val filters = JSONUtils.deserialize[List[Map[String, AnyRef]]](JSONUtils.serialize(mapQuery("filters")))
-    val channelId = s"""{\"channel\":\"$channel\"}""".stripMargin
+    val channelId = s"""{"channel":"$channel"}""".stripMargin
     val updatedFilters = filters.map(f => {
       f map {
-        case ("value","channelId") => "value" -> JSONUtils.serialize(channelId)
+        case ("value","channelId") => "value" -> channelId
         case ("dimension","channel") => "dimension" -> "originData"
         case x => x
       }
