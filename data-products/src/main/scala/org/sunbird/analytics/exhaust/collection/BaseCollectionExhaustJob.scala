e185c127019505f7e682d8443496790303210a96
Manjunath Davanam <>
2021-03-30 09:34:54 +0530

    Issue SB-23646 fix: Test case enhancements

--- data-products/src/main/scala/org/sunbird/analytics/exhaust/collection/BaseCollectionExhaustJob.scala
+++ data-products/src/main/scala/org/sunbird/analytics/exhaust/collection/BaseCollectionExhaustJob.scala
@@ -250,10 +250,8 @@ trait BaseCollectionExhaustJob extends BaseReportsJob with IJob with OnDemandExh
   /** START - Utility Methods */
 
   def getFilePath(batchId: String, requestId: String)(implicit config: JobConfig): String = {
-    if(requestId.nonEmpty)
-      getReportPath() + requestId.concat("/" ) + batchId + "_" + getReportKey() + "_" + getDate() // To Handle the OnDemand Mode
-    else
-      getReportPath() + batchId + "_" + getReportKey() + "_" + getDate() // To handle the StandAlone Mode
+    val requestIdPath = if (requestId.nonEmpty) requestId.concat("/") else ""
+    getReportPath() + requestIdPath + batchId + "_" + getReportKey() + "_" + getDate()
   }
 
   def getDate(): String = {
