5f27e094fa62bcdb6570f60563b1ca3abe264a24
Utkarsha Kapoor <utkit14@gmail.com>
2021-04-01 17:06:32 +0530

    Issue #SB-23835: Test Coverage improved for StateAdminGeoReportJob

--- data-products/src/test/scala/org/sunbird/analytics/job/report/TestStateAdminGeoReportJob.scala
+++ data-products/src/test/scala/org/sunbird/analytics/job/report/TestStateAdminGeoReportJob.scala
@@ -9,7 +9,7 @@ import org.sunbird.cloud.storage.conf.AppConf
 import java.io.File
 
 import org.ekstep.analytics.framework.{Fetcher, FrameworkContext, JobConfig}
-import org.ekstep.analytics.framework.util.HadoopFileUtil
+import org.ekstep.analytics.framework.util.{HadoopFileUtil, JSONUtils}
 
 class TestStateAdminGeoReportJob extends SparkSpec(null) with MockFactory {
 
@@ -60,7 +60,7 @@ class TestStateAdminGeoReportJob extends SparkSpec(null) with MockFactory {
     assert(geoSummaryDistrict.exists() === true)
   }
 
-  ignore should "execute dispatcher" in {
+  it should "execute dispatcher" in {
     implicit val fc = new FrameworkContext()
     val reportDF = StateAdminGeoReportJob.generateGeoReport()(spark, fc)
 
@@ -68,6 +68,21 @@ class TestStateAdminGeoReportJob extends SparkSpec(null) with MockFactory {
       "adhoc_scripts_output_dir" -> "/mount/portal_data")
 
     val jobConfig = JobConfig(Fetcher("local", None, None), None, None, "StateAdminJob", Some(modelParams), None, Some(4), Some("TestExecuteDispatchder"))
-    StateAdminGeoReportJob.generateDistrictZip(reportDF, jobConfig)
+    println("jobconfig: " + JSONUtils.serialize(jobConfig))
+    the[Exception] thrownBy {
+      StateAdminGeoReportJob.generateDistrictZip(reportDF, jobConfig)
+    } should have message "District level zip generation failed with exit code 127"
+  }
+
+  it should "execute main method" in {
+    implicit val fc = new FrameworkContext()
+    val modelParams = Map[String, AnyRef]("adhoc_scripts_virtualenv_dir" -> "/mount/venv",
+      "adhoc_scripts_output_dir" -> "/mount/portal_data")
+
+    val jobConfig = JobConfig(Fetcher("local", None, None), None, None, "StateAdminJob", Some(modelParams), None, Some(4), Some("TestExecuteDispatchder"))
+    val strConfig = JSONUtils.serialize(jobConfig)
+    the[Exception] thrownBy {
+      StateAdminGeoReportJob.main(strConfig)
+    } should have message "District level zip generation failed with exit code 127"
   }
 }
