c036238022e9bd9163914395f41c3f7a58d4c796
kumarks1122 <kumar.kandasamy@tarento.com>
2021-04-22 14:17:27 +0530

    Issue #SB-24151 | Test case and ProgressExhaustJob changes added

--- data-products/src/main/scala/org/sunbird/analytics/audit/CollectionReconciliationJob.scala
+++ data-products/src/main/scala/org/sunbird/analytics/audit/CollectionReconciliationJob.scala
@@ -207,9 +207,12 @@ object CollectionReconciliationJob extends optional.Application with IJob with B
     val enrolmentDF = enrolmentCourseJoinedDF.filter(col("completed") === "Yes").select("userid", "courseid", "batchid");
     val enrolmentRDD = enrolmentDF.rdd.repartition(30).map(f => UserEnrolment(f.getString(0), f.getString(1), f.getString(2)))
 
-    val joinedRdd = enrolmentRDD.joinWithCassandraTable("sunbird_courses", "user_content_consumption", SomeColumns("contentid", "status", "lastcompletedtime"), SomeColumns("userid", "batchid", "courseid"))
-    val finalRDD = joinedRdd.filter(f => f._2.getIntOption(1).getOrElse(0) == 2).map(f => (f._1.userid, f._1.batchid, f._1.courseid, f._2.getString(2)))
-    val enrolmentJoinedDF = finalRDD.toDF().withColumnRenamed("_1", "userid").withColumnRenamed("_2", "batchid").withColumnRenamed("_3", "courseid").withColumnRenamed("_4", "lastcompletedtime")
+    val joinedRdd = enrolmentRDD.joinWithCassandraTable("sunbird_courses", "user_content_consumption", SomeColumns("contentid", "status", "lastcompletedtime", "last_completed_time"), SomeColumns("userid", "batchid", "courseid"))
+    val finalRDD = joinedRdd.filter(f => f._2.getIntOption(1).getOrElse(0) == 2).map(f => (f._1.userid, f._1.batchid, f._1.courseid, f._2.getString(2), f._2.getString(3)))
+    val enrolmentJoinedDF = finalRDD.toDF().withColumnRenamed("_1", "userid").withColumnRenamed("_2", "batchid").withColumnRenamed("_3", "courseid")
+      .withColumnRenamed("_4", "lastcompletedtime").withColumnRenamed("_5", "last_completed_time")
+      .withColumn("lastcompletedtime", UDFUtils.getLatestValue(col("last_completed_time"), col("lastcompletedtime")))
+      .select("userid", "batchid", "courseid", "lastcompletedtime")
     val enrolmentWithRevisedDtDF = enrolmentJoinedDF.groupBy("userid", "batchid", "courseid").agg(max(col("lastcompletedtime")).alias("lastreviseddate")).withColumn("lastrevisedon", unix_timestamp(col("lastreviseddate"), "yyyy-MM-dd HH:mm:ss:SSS"))
     enrolmentWithRevisedDtDF.join(courseBatchDF, "batchid")
       .withColumn("endedon", unix_timestamp(col("enddate"), "yyyy-MM-dd"))
