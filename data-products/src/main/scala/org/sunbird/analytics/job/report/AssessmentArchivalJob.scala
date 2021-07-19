b32d1e6ef69d28718fca4c9780c1a3a2428fc816
Manjunath Davanam <manjunath@sanketika.in>
2021-07-19 16:15:06 +0530

    Issue SB-25481 feat:  Assessment archival data product archive the data per batch wise

--- data-products/src/main/scala/org/sunbird/analytics/job/report/AssessmentArchivalJob.scala
+++ data-products/src/main/scala/org/sunbird/analytics/job/report/AssessmentArchivalJob.scala
@@ -55,8 +55,7 @@ object AssessmentArchivalJob extends optional.Application with IJob with BaseRep
 
   // $COVERAGE-ON$
   def archiveData(sparkSession: SparkSession, fetchData: (SparkSession, Map[String, String], String, StructType) => DataFrame, jobConfig: JobConfig): Array[Map[String, Any]] = {
-    val modelParams = jobConfig.modelParams.get
-    val batches: List[String] = JSONUtils.deserialize[List[String]](modelParams.getOrElse("batches", "").asInstanceOf[String])
+    val batches: List[String] = AppConf.getConfig("assessment.batches").split(",").toList
     val assessmentData: DataFrame = getAssessmentData(sparkSession, fetchData, batches)
       .withColumn("updated_on", to_timestamp(col("updated_on")))
       .withColumn("year", year(col("updated_on")))
