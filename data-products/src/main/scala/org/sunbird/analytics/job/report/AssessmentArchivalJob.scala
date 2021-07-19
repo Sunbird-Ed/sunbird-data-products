22f76e9d2ef6319fb74d13e05d31847de7a36cba
Manjunath Davanam <manjunath@sanketika.in>
2021-07-19 16:25:29 +0530

    Issue SB-25481 feat:  Assessment archival data product archive the data per batch wise

--- data-products/src/main/scala/org/sunbird/analytics/job/report/AssessmentArchivalJob.scala
+++ data-products/src/main/scala/org/sunbird/analytics/job/report/AssessmentArchivalJob.scala
@@ -79,7 +79,7 @@ object AssessmentArchivalJob extends optional.Application with IJob with BaseRep
 
   def getAssessmentData(spark: SparkSession, fetchData: (SparkSession, Map[String, String], String, StructType) => DataFrame, batchIds: List[String]): DataFrame = {
     val assessmentDF = fetchData(spark, assessmentAggDBSettings, cassandraUrl, new StructType())
-    if (batchIds.nonEmpty) assessmentDF.filter(col("batch_id").isin(batchIds)) else assessmentDF
+    if (batchIds.nonEmpty) assessmentDF.filter(col("batch_id").isInCollection(batchIds)) else assessmentDF
   }
 
   def deleteRecords(sparkSession: SparkSession, keyspace: String, table: String): Unit = {
