1a5d03dcab53e8f5c6d06e692f7d055720e1531b
Manjunath Davanam <manjunath@sanketika.in>
2021-08-25 17:00:11 +0530

    Issue SB-24793 feat:  Score migration testcase issue fix

--- data-products/src/test/scala/org/sunbird/analytics/audit/TestAssessmentScoreCorrectionJob.scala
+++ data-products/src/test/scala/org/sunbird/analytics/audit/TestAssessmentScoreCorrectionJob.scala
@@ -28,14 +28,14 @@ class TestAssessmentScoreCorrectionJob extends BaseReportSpec with MockFactory {
   it should "Should able correct the records" in {
     implicit val mockFc: FrameworkContext = mock[FrameworkContext]
     implicit val sc: SparkContext = spark.sparkContext
-    val strConfig = """{"search":{"type":"none"},"model":"org.sunbird.analytics.job.report.AssessmentScoreCorrectionJob","modelParams":{"assessment.score.correction.batches":["batch-00001"],"isDryRunMode":true,"csvPath":"src/test/resources/score-metrics-migration-job/","store":"local","sparkCassandraConnectionHost":"{{ core_cassandra_host }}","fromDate":"$(date --date yesterday '+%Y-%m-%d')","toDate":"$(date --date yesterday '+%Y-%m-%d')"},"parallelization":8,"appName":"Assessment Score Correction"}""".stripMargin
+    val strConfig = """{"search":{"type":"none"},"model":"org.sunbird.analytics.job.report.AssessmentScoreCorrectionJob","modelParams":{"contentReadAPI":"https://dev.sunbirded.org/api/content/v1/read/","assessment.score.correction.batches":["batch-00001"],"isDryRunMode":true,"csvPath":"src/test/resources/score-metrics-migration-job/","store":"local","sparkCassandraConnectionHost":"{{ core_cassandra_host }}","fromDate":"$(date --date yesterday '+%Y-%m-%d')","toDate":"$(date --date yesterday '+%Y-%m-%d')"},"parallelization":8,"appName":"Assessment Score Correction"}""".stripMargin
     implicit val jobConfig: JobConfig = JSONUtils.deserialize[JobConfig](strConfig)
     val reportData = AssessmentScoreCorrectionJob.processBatches()
     reportData.foreach(data => {
       data("batch_id").asInstanceOf[String] should be("batch-00001")
-      data("total_records").asInstanceOf[Long] should be(3)
-      data("content_id").asInstanceOf[String] should be("do_313026415363981312122")
-      data("total_questions").asInstanceOf[Int] should be(5)
+      data("total_records").asInstanceOf[Long] should be(2)
+      data("content_id").asInstanceOf[String] should be("do_11307972307046400011917")
+      data("total_questions").asInstanceOf[Int] should be(4)
     })
   }
 }
