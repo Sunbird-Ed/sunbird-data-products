c4ed59f1a8c4dba0a2f98d92ba6cf84981716f6f
Surabhi <surabhi.mahawar@aurigait.com>
2022-03-23 11:51:16 +0530

    changes

--- data-products/src/main/scala/org/sunbird/analytics/exhaust/uci/UCIResponseExhaustJob.scala
+++ data-products/src/main/scala/org/sunbird/analytics/exhaust/uci/UCIResponseExhaustJob.scala
@@ -21,11 +21,11 @@ object UCIResponseExhaustJob extends optional.Application with BaseUCIExhaustJob
 
   private val columnsOrder = List("Message ID", "Conversation ID", "Conversation Name", "Device ID", "Question Id", "Question Type",
     "Question Title", "Question Description", "Question Duration", "Question Score", "Question Max Score",
-    "Question Options", "Question Response", "Question Details", "Timestamp")
+    "Question Options", "Question Response", "XPath", "EOF", "Timestamp")
   private val columnMapping = Map("mid" -> "Message ID", "conversation_id" -> "Conversation ID", "conversation_name" -> "Conversation Name",
     "device_id" -> "Device ID", "question_id" -> "Question Id", "question_type" -> "Question Type", "question_title" -> "Question Title",
     "question_description" -> "Question Description", "question_duration" -> "Question Duration", "question_score" -> "Question Score", "question_maxscore" -> "Question Max Score",
-    "question_response" -> "Question Response", "question_option" -> "Question Options", "question_details" => "Question Details", "timestamp" -> "Timestamp")
+    "question_response" -> "Question Response", "question_option" -> "Question Options", "xpath" => "XPath", "eof" => "EOF", "timestamp" -> "Timestamp")
 
   override def process(conversationId: String, telemetryDF: DataFrame, conversationDF: DataFrame)(implicit spark: SparkSession, fc: FrameworkContext, config: JobConfig): DataFrame = {
 
@@ -46,7 +46,8 @@ object UCIResponseExhaustJob extends optional.Application with BaseUCIExhaustJob
         .withColumn("question_maxscore", col("edata.item.maxscore"))
         .withColumn("question_response", to_json(col("edata.resvalues")))
         .withColumn("question_option", to_json(col("edata.item.params")))
-        .withColumn("question_details", to_json(col("context.cdata")))
+        .withColumn("xpath", col("context.rollup.l3"))
+        .withColumn("eof", col("context.rollup.l4"))
         .withColumn("mid", col("mid"))
         .withColumn("timestamp", col("@timestamp"))
         .join(userDF, Seq("device_id"), "inner")
