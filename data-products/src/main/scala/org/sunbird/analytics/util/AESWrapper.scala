2345fa09f6ff191a94a90d415b9596531a6c453a
kumarks1122 <kumar.kandasamy@tarento.com>
2022-06-17 20:31:29 +0530

    Issue #SB-30306 | Base64 Decrypt fixes added

--- data-products/src/main/scala/org/sunbird/analytics/util/AESWrapper.scala
+++ data-products/src/main/scala/org/sunbird/analytics/util/AESWrapper.scala
@@ -2,7 +2,7 @@ package org.sunbird.analytics.util
 
 import org.ekstep.analytics.framework.conf.AppConf
 
-import java.util.Base64
+import org.apache.commons.codec.binary.Base64
 import javax.crypto.Cipher
 import javax.crypto.spec.SecretKeySpec
 
@@ -16,7 +16,7 @@ object AESWrapper {
     val c = Cipher.getInstance(ALGO)
     c.init(Cipher.ENCRYPT_MODE, key)
     val encVal = c.doFinal(Data.getBytes)
-    Base64.getEncoder.encodeToString(encVal)
+    Base64.encodeBase64String(encVal)
   }
 
   def decrypt(strToDecrypt: String, secret: Option[String]): String = {
@@ -24,7 +24,7 @@ object AESWrapper {
       val key = generateKey(secret.getOrElse(AppConf.getConfig("uci_encryption_secret")))
       val cipher = Cipher.getInstance(ALGO)
       cipher.init(Cipher.DECRYPT_MODE, key)
-      new String(cipher.doFinal(Base64.getDecoder.decode(strToDecrypt)))
+      new String(cipher.doFinal(Base64.decodeBase64(strToDecrypt)))
     } catch {
       case e: Exception =>
         System.out.println("Error while decrypting: " + e.toString)
@@ -34,17 +34,17 @@ object AESWrapper {
 
   @throws[Exception]
   private def generateKey(secret: String) = {
-    val decoded = Base64.getDecoder.decode(secret.getBytes)
+    val decoded = Base64.decodeBase64(secret.getBytes)
     new SecretKeySpec(decoded, ALGO)
   }
 
   def decodeKey(str: String): String = {
-    val decoded = Base64.getDecoder.decode(str.getBytes)
+    val decoded = Base64.decodeBase64(str.getBytes)
     new String(decoded)
   }
 
   def encodeKey(str: String): String = {
-    val encoded = Base64.getEncoder.encode(str.getBytes)
+    val encoded = Base64.decodeBase64(str.getBytes)
     new String(encoded)
   }
 }
