7461fda7e537077deaa94a81be50d48db36ea076
Deepak Devadathan <deepak.devadathan@oracle.com>
2023-04-18 13:22:50 +1000

    updated spark hd configuration
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- data-products/src/main/scala/org/sunbird/analytics/exhaust/BaseReportsJob.scala
+++ data-products/src/main/scala/org/sunbird/analytics/exhaust/BaseReportsJob.scala
@@ -61,7 +61,7 @@ trait BaseReportsJob {
         // spark.sparkContext.hadoopConfiguration.set("fs.s3n.endpoint", AppConf.getConfig(storageEndpoint));
         spark.sparkContext.hadoopConfiguration.set("fs.s3n.awsAccessKeyId", storageKey);
         spark.sparkContext.hadoopConfiguration.set("fs.s3n.awsSecretAccessKey", storageSecret);
-        spark.sparkContext.hadoopConfiguration.set("fs.s3n.endpoint", storageEndpoint);
+        // spark.sparkContext.hadoopConfiguration.set("fs.s3n.endpoint", storageEndpoint);
       case "azure" =>
         val storageKeyValue = AppConf.getConfig(storageKey);
         spark.sparkContext.hadoopConfiguration.set("fs.azure", "org.apache.hadoop.fs.azure.NativeAzureFileSystem")
