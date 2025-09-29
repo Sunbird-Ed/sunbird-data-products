c5a78e23de33d3d4b98393ddaee3668f17b63e97
Mahesh Kumar Gangula <maheshkumargangula@users.noreply.github.com>
2025-09-29 17:12:20 +0530

    feat: refactoring the code.

--- data-products/src/main/scala/org/sunbird/analytics/job/report/ContentDataIndexerJob.scala
+++ data-products/src/main/scala/org/sunbird/analytics/job/report/ContentDataIndexerJob.scala
@@ -8,16 +8,18 @@ import org.ekstep.analytics.framework.Level.INFO
 import org.ekstep.analytics.framework.conf.AppConf
 import org.ekstep.analytics.framework.util.DatasetUtil.extensions
 import org.ekstep.analytics.framework.util.{CommonUtil, JSONUtils, JobLogger, RestUtil}
-import org.ekstep.analytics.framework.{FrameworkContext, IJob, JobConfig}
+import org.ekstep.analytics.framework.{FrameworkContext, IJob, JobConfig, Dispatcher, OutputDispatcher}
 import org.ekstep.analytics.util.Constants
 import org.joda.time.DateTimeZone
 import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
 import org.sunbird.analytics.exhaust.collection.UDFUtils
+import org.elasticsearch.spark._
 
 
 object ContentDataIndexerJob extends IJob with BaseReportsJob {
 
     implicit val className: String = "org.sunbird.analytics.job.report.ContentDataIndexerJob"
+    val jobName = "ContentDataIndexerJob"
 
     // $COVERAGE-OFF$ Disabling scoverage for main and execute method
     override def main(config: String)(implicit sc: Option[SparkContext] = None, fc: Option[FrameworkContext] = None) {
@@ -30,21 +32,22 @@ object ContentDataIndexerJob extends IJob with BaseReportsJob {
 
             // Get configurations from JobConfig
             // modelParams should have sparkElasticsearchConnectionHost, elasticsearchQueryIndex, elasticsearchQueryType, elasticsearchQueryJsonString, elasticsearchReadFields
-            val modelParams = config.modelParams.getOrElse(Map[String, Option[AnyRef]]());
-            val index =  modelParams.getOrElse("elasticsearchQueryIndex", "compositesearch")
-            val docType =  modelParams.get("elasticsearchQueryType")
+            val modelParams = jobConfig.modelParams.getOrElse(Map[String, AnyRef]())
+            val index =  modelParams.getOrElse("elasticsearchQueryIndex", "compositesearch").asInstanceOf[String]
+            val docType =  modelParams.getOrElse("elasticsearchQueryType", "").asInstanceOf[String]
             // query to fetch all 'Live' status contents of objectType 'Content'
-            val query = modelParams.getOrElse("elasticsearchQueryJsonString", "{\"query\":{\"bool\":{\"must\":[{\"match\":{\"status\":{\"query\":\"Live\"}}},{\"match\":{\"objectType\":{\"query\":\"Content\"}}}]}}}")
+            val query = modelParams.getOrElse("elasticsearchQueryJsonString", "{\"query\":{\"bool\":{\"must\":[{\"match\":{\"status\":{\"query\":\"Live\"}}},{\"match\":{\"objectType\":{\"query\":\"Content\"}}}]}}}").asInstanceOf[String]
             val fieldsCsv = modelParams.get("elasticsearchReadFields") // Not being used currently
 
             val indexPath = if (docType.trim.nonEmpty) s"$index/${docType.trim}" else index
 
             // Fetch from Elasticsearch
-            val jsonRdd = sc.esJsonRDD(indexPath, query).map(_._2)
+            val jsonRdd = spark.sparkContext.esJsonRDD(indexPath, query).map(_._2)
             
             // Publish records to Kafka using Dispatcher
-            val topic = modelParams.getOrElse("topic", "dev.ingest")
-            val bootstrap = modelParams.getOrElse("brokerList", "")
+            val topic = modelParams.getOrElse("topic", "dev.ingest").asInstanceOf[String]
+            val brokerList = modelParams.getOrElse("brokerList", "").asInstanceOf[String]
+            implicit val scForDispatcher: SparkContext = spark.sparkContext
             OutputDispatcher.dispatch(Dispatcher("kafka", Map("brokerList" -> brokerList, "topic" -> topic)), jsonRdd);
             
             jsonRdd.unpersist()
