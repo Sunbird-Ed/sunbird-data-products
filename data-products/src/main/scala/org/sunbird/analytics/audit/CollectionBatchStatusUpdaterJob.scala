e30fe389ac24d4b9c44c75a7760f7af0cd054563
Mahesh Kumar Gangula <6985261+maheshkumargangula@users.noreply.github.com>
2025-10-14 11:19:44 +0530

    feat: Collection Batch Status Updater Job implementation.

---
+++ data-products/src/main/scala/org/sunbird/analytics/audit/CollectionBatchStatusUpdaterJob.scala
@@ -0,0 +1,108 @@
+package org.sunbird.analytics.audit
+
+import com.datastax.spark.connector.cql.CassandraConnectorConf
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.functions.{col, lit, unix_timestamp, when}
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+import org.ekstep.analytics.framework.Level.{ERROR, INFO}
+import org.ekstep.analytics.framework.conf.AppConf
+import org.ekstep.analytics.framework.util.{CommonUtil, JSONUtils, JobLogger}
+import org.ekstep.analytics.framework.{FrameworkContext, IJob, JobConfig}
+import org.sunbird.analytics.job.report.BaseReportsJob
+import org.apache.spark.sql.cassandra._
+
+import java.text.SimpleDateFormat
+import java.util.{Date, TimeZone}
+import org.apache.spark
+
+case class CollectionBatchStatusMetrics(unStarted: Long, inProgress: Long, completed: Long)
+
+object CollectionBatchStatusUpdaterJob extends IJob with BaseReportsJob {
+  implicit val className: String = "org.sunbird.analytics.audit.CollectionBatchStatusUpdaterJob"
+  val cassandraFormat = "org.apache.spark.sql.cassandra"
+  private val collectionBatchDBSettings = Map("table" -> "batches", "keyspace" -> "sb_collection_tracking", "cluster" -> "LMSCluster")
+
+  // $COVERAGE-OFF$ Disabling scoverage for main and execute method
+  override def main(config: String)(implicit sc: Option[SparkContext], fc: Option[FrameworkContext]): Unit = {
+    val jobName: String = "CollectionBatchStatusUpdaterJob"
+    implicit val jobConfig: JobConfig = JSONUtils.deserialize[JobConfig](config)
+    JobLogger.init(jobName)
+    JobLogger.start(s"$jobName started executing", Option(Map("config" -> config, "model" -> jobName)))
+    implicit val frameworkContext: FrameworkContext = getReportingFrameworkContext()
+    implicit val spark: SparkSession = openSparkSession(jobConfig)
+    implicit val sc: SparkContext = spark.sparkContext
+    try {
+      spark.setCassandraConf("LMSCluster", CassandraConnectorConf.ConnectionHostParam.option(AppConf.getConfig("sunbird.courses.cluster.host")))
+      val res = CommonUtil.time(execute(fetchData))
+      JobLogger.end(s"$jobName completed execution", "SUCCESS", Option(Map(
+        "time-taken" -> res._1,
+        "un-started" -> res._2.unStarted,
+        "in-progress" -> res._2.inProgress,
+        "completed" -> res._2.completed
+      )))
+    } catch {
+      case ex: Exception =>
+        JobLogger.log(ex.getMessage, None, ERROR);
+        JobLogger.end(s"$jobName execution failed", "FAILED", Option(Map("model" -> jobName, "statusMsg" -> ex.getMessage)));
+    }
+    finally {
+      frameworkContext.closeContext()
+      spark.close()
+    }
+  }
+
+  // $COVERAGE-ON$ Disabling scoverage for main and execute method
+  def execute(fetchData: (SparkSession, Map[String, String], String, StructType) => DataFrame)(implicit spark: SparkSession, fc: FrameworkContext, config: JobConfig, sc: SparkContext): CollectionBatchStatusMetrics = {
+    val collectionBatchDF = getCollectionBatchDF(fetchData).persist()
+    val metrics = Map("un-started" -> collectionBatchDF.filter(col("status") === 0).count(), "in-progress" -> collectionBatchDF.filter(col("status") === 1).count(), "completed" -> collectionBatchDF.filter(col("status") === 2).count())
+    JobLogger.log(s"Collection Batch status metrics before updating the table", Option(metrics), INFO)
+    val res = updateBatchStatus(config, collectionBatchDF)
+    collectionBatchDF.unpersist()
+    res
+  }
+
+  def updateBatchStatus(updaterConfig: JobConfig, collectionBatchDF: DataFrame)(implicit sc: SparkContext, spark: SparkSession): CollectionBatchStatusMetrics = {
+    val currentDate = getDateFormat().format(new Date)
+    JobLogger.log(s"Update batch status started", None, INFO)
+    val computedDF = collectionBatchDF.withColumn("updated_status",
+      when(unix_timestamp(lit(currentDate), "yyyy-MM-dd").gt(unix_timestamp(col("enddate"), "yyyy-MM-dd")), 2).otherwise(
+        when(unix_timestamp(lit(currentDate), "yyyy-MM-dd").geq(unix_timestamp(col("startdate"), "yyyy-MM-dd")), 1).otherwise(col("status"))
+      ))
+    val finalDF = computedDF.filter(col("updated_status") =!= col("status"))
+      .drop("status").withColumnRenamed("updated_status", "status")
+    if (!finalDF.isEmpty) {
+      JobLogger.log(s"Writing records into database", None, INFO)
+      finalDF.select("courseid", "batchid", "status").write.format("org.apache.spark.sql.cassandra").options(collectionBatchDBSettings ++ Map("confirm.truncate" -> "false")).mode(SaveMode.Append).save()
+    } else {
+      JobLogger.log("No records found to update the db", None, INFO)
+    }
+    CollectionBatchStatusMetrics(finalDF.filter(col("status") === 0).count(), finalDF.filter(col("status") === 1).count(), finalDF.filter(col("status") === 2).count())
+  }
+
+  def getCollectionBatchDF(fetchData: (SparkSession, Map[String, String], String, StructType) => DataFrame)(implicit spark: SparkSession): DataFrame = {
+    val convertDate = spark.udf.register("convertDate", convertDateFn)
+    fetchData(spark, collectionBatchDBSettings, cassandraFormat, new StructType())
+      .withColumn("startdate", convertDate(col("start_date")))
+      .withColumn("enddate", convertDate(col("end_date")))
+      .withColumn("enrollmentenddate", convertDate(col("enrollment_enddate")))
+      .select("courseid", "batchid", "startdate", "name", "enddate", "enrollmentenddate", "enrollmenttype", "createdfor", "status").cache()
+  }
+
+
+  def getDateFormat(): SimpleDateFormat = {
+    val dateFormatter = new SimpleDateFormat("yyyy-MM-dd")
+    dateFormatter.setTimeZone(TimeZone.getTimeZone("IST"))
+    dateFormatter
+  }
+
+  def convertDateFn : String => String = (date: String) => {
+    Option(date).map(x => {
+      val utcDateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+      utcDateFormatter.setTimeZone(TimeZone.getTimeZone("UTC"))
+      getDateFormat().format(utcDateFormatter.parse(x))
+    }).orNull
+  }
+
+}
+
